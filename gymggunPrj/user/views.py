7208b54abce6980da35947ba5cf47274d514a1b6
BaeJiYeong <jy111099@gmail.com>
2021-06-29 17:46:06 +0900

    🏷️ Add signup and login

--- gymggunPrj/user/views.py
+++ gymggunPrj/user/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+import json
+from django.contrib import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django import views
+from django.db.models.fields import json
+from django.shortcuts import render, redirect
+from django.views import View
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import User
+
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password1'],
+                email=request.POST['email'],
+            )
+            auth.login(request, user)
+            return redirect('/')
+        return render(request, 'signup.html')
+    return render(request, 'signup.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': '아이디나 비밀번호가 잘못되었습니다.'})
+
+    else:
+        return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
+
+
+def home(request):
+    return render(request, 'home.html')
